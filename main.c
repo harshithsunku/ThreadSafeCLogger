2b7c3aa1fbf60a64cf4de91dcf27ad9fa2636e09
Harshith Sunku <harshithsunku@gmail.com>
2023-12-23 00:05:43 +0530

    Initial commit

---
+++ main.c
@@ -0,0 +1,31 @@
+/**
+ * @file        main.c
+ * @brief       ThreadSafeCLogger: A high-performance, thread-safe logging library for C applications.
+ * @author      Harshith Sunku
+ * @date        [12/22/2023]
+ * @version     1.0
+ * @website     https://www.harshith.in/ | https://harshithsunku.in/
+ * @repository  https://github.com/harshithsunku/ThreadSafeCLogger
+ *
+ * @note        This file is part of ThreadSafeCLogger, which provides multi-threaded 
+ *              logging capabilities in a thread-safe manner.
+ * @details     Further details about the implementation can be found in
+ *              the README or the official documentation at the above website/github.
+ * 
+ * ThreadSafeCLogger is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * ThreadSafeCLogger is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with ThreadSafeCLogger. If not, see <https://www.gnu.org/licenses/>.
+ *
+ * @bugs        No known bugs at the time of writing.
+ * @todo        Features or fixes planned for future versions.
+ */
+
